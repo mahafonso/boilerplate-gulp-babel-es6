20e8a98294dcc24248391c2d11c638ae6e3a9479
Marcela Afonso <marcela.bafonso3@gmail.com>
2020-06-22 17:31:53 -0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const gulp = require('gulp'),
     rename = require('gulp-rename'),
     babel = require('gulp-babel'),
     clean = require('gulp-clean'),
+    connect = require('gulp-connect'),
     path = require('path'),
     autoprefixer = require('autoprefixer'),
     cssnano = require('cssnano'),
@@ -12,11 +13,18 @@ const gulp = require('gulp'),
     util = require('gulp-util');
 
 const paths = {
+    html: './src/*.html',
     scripts: 'src/scripts/**/*.js',
     webpack: 'src/scripts/*.js',
     styles: 'src/styles/**/*.scss',
     sass: 'src/styles/*.scss'
 };
+
+gulp.task('html', function() {
+    gulp.src(paths.html)
+        .pipe(gulp.dest('build/files'))
+        .pipe(connect.reload());
+});
 
 gulp.task('styles', () => {
     return gulp.src(paths.sass)
@@ -60,17 +68,24 @@ gulp.task('scripts', () => {
         .pipe(browserSync.reload({stream: true}));
 });
 
+gulp.task('connect', () => {
+    connect.server({
+		root: 'src',
+		open: false,
+		port: 8000,
+		livereload: true
+	}, () => {
+		browserSync({
+          proxy: '127.0.0.1:8000',
+          open: true
+		});
+	  })
+});
+
 gulp.task('watch', () => {
-    browserSync({
-        server: {
-            baseDir: './build'
-        },
-        files: './build/files',
-        open: false
-    });
-    
-    gulp.watch(paths.scripts, gulp.series('scripts'));
+    gulp.watch(paths.styles, gulp.series('html'));
     gulp.watch(paths.styles, gulp.series('styles'));
+    gulp.watch(paths.scripts, gulp.series('scripts'));
 });
 
 gulp.task('clean', () => {
@@ -81,4 +96,4 @@ gulp.task('clean', () => {
 
 gulp.task('deploy', gulp.parallel('styles', 'scripts'));
 
-gulp.task('default', gulp.series('clean', gulp.parallel('styles', 'scripts', 'watch')));
+gulp.task('default', gulp.series('clean', gulp.parallel('html', 'styles', 'scripts', 'connect', 'watch')));
